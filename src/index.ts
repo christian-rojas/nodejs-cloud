19b4ec8d0f75afc20e17cd9ef2ae017329ab8905
christian-rojas <christian.ici17@gmail.com>
2022-10-14 01:34:07 -0300

    add redis to docker compose

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,21 @@ import express, { Express, Request, Response } from 'express';
 import os from 'os'
 import { main, create, getPets } from './services'
 import bodyparser from 'body-parser'
+import { createClient } from 'redis';
+
+const url = process.env.REDIS_URL as string
+// const client = createClient({url: `redis://${url}` });
+const client = createClient({url: `redis://redis:6379` });
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+// client.on('connect', () => console.log('successfully connected'));
+// const connectionString = () => {
+//   return client.connect()
+// }
+// const connect = () => client.connect();
+// console.log(async() => await connectionString());
+
+
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
@@ -10,9 +25,8 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 app.get('/', async (req: Request, res: Response) => {
-  const pets = await getPets()
-  // console.log(pets);
-  res.send(`node cloud js - you reach ${os.hostname()} - ${ pets } /`);
+  // const pets = await getPets()
+  res.send(`node cloud js - you reach ${os.hostname()}`);
 });
 
 app.post('/create', async (req: Request, res: Response) => {
@@ -21,6 +35,12 @@ app.post('/create', async (req: Request, res: Response) => {
 })
 
 app.listen({port, host: '0.0.0.0'}, async () => {
-  await main()
+  // await main()
+  await client.connect();
+  console.time("start")
+  await client.set('key', 'mortis');
+  const value = await client.get('key');
+  console.timeEnd("start")
+  console.log(value);
   console.log(`⚡️[server]: Server is running on port: ${port}`);
 });
