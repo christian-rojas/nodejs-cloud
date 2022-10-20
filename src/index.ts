4dae551b23047c342f11b121de67b748d1a3d1b4
christian-rojas <christian.ici17@gmail.com>
2022-10-20 16:30:11 -0300

    no redis connection

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import express, { Express, Request, Response } from 'express'
 import os from 'os'
 import { main, create, getPets } from './services'
 import bodyparser from 'body-parser'
-import { createClient } from 'redis'
+// import { createClient } from 'redis'
 
 // const url = process.env.REDIS_URL as string
 // const client = createClient({url: `redis://${url}` });
-const client = createClient({ url: 'redis://redis:6379' })
+// const client = createClient({ url: 'redis://redis:6379' })
 
-client.on('error', (err) => console.log('Redis Client Error', err))
+// client.on('error', (err) => console.log('Redis Client Error', err))
 // client.on('connect', () => console.log('successfully connected'));
 // const connectionString = () => {
 //   return client.connect()
@@ -37,12 +37,12 @@ app.post('/create', async (req: Request, res: Response) => {
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 app.listen({ port, host: '0.0.0.0' }, async () => {
   await main()
-  await client.connect()
-  console.time('start')
-  console.time('mortis')
-  await client.set('key', 'mortis')
-  const value = await client.get('key')
-  console.log(value)
-  console.timeEnd('mortis')
+  // await client.connect()
+  // console.time('start')
+  // console.time('mortis')
+  // await client.set('key', 'mortis')
+  // const value = await client.get('key')
+  // console.log(value)
+  // console.timeEnd('mortis')
   console.log(`[server]: Server is running on port: ${port}`)
 })
